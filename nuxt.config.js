19c94f893724626e8d5595c4dfbffdff7589666f
kouta1612 <kouta1612world69@gmail.com>
2021-01-23 15:57:13 +0900

    fetch albums from iTunes

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,39 +1,40 @@
 module.exports = {
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
-    title: 'nuxt-itunes-search',
+    title: "My iTunes Search Program",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "Nuxt.js project" }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
   /*
-  ** Customize the progress bar color
-  */
-  loading: { color: '#3B8070' },
+   ** Customize the progress bar color
+   */
+  plugins: ["~plugins/vuetify.js"],
+  buildModules: ["@nuxtjs/vuetify"],
+  // css: ["~assets/app.sty1"],
+  loading: { color: "#3B8070" },
   /*
-  ** Build configuration
-  */
+   ** Build configuration
+   */
   build: {
+    vendor: ["axios", "vuetify"],
     /*
-    ** Run ESLint on save
-    */
-    extend (config, { isDev, isClient }) {
-      if (isDev && isClient) {
-        config.module.rules.push({
-          enforce: 'pre',
-          test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
-          exclude: /(node_modules)/
-        })
-      }
+     ** Run ESLint on save
+     */
+    extend(config, { isDev, isClient }) {
+      // if (isDev && isClient) {
+      //   config.module.rules.push({
+      //     enforce: "pre",
+      //     test: /\.(js|vue)$/,
+      //     loader: "eslint-loader",
+      //     exclude: /(node_modules)/
+      //   });
+      // }
     }
   }
-}
-
+};
---
+++ pages/results/_id.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1>Hello From results route {{ $route.params.id }}</h1>
+        {{ albumDatas }}
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+    // middleware: 'search'
+    asyncData({params}) {
+        return axios
+        .get(`https://itunes.apple.com/search?term=${params.id}&entity=album`)
+        .then(response => {
+            return { albumDatas: response.data.results }
+        });
+    },
+}
+</script>
